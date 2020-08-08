d211f882eff6adf5efb37144f9e4eace94c0b66a
gadnlino <guiavenas@gmail.com>
2020-08-07 23:24:10 -0300

    Adicionando lambdas stmj-get-person-masses e stmj-get-mass-people

--- stmj-create-mass/index.js
+++ stmj-create-mass/index.js
@@ -1,6 +1,17 @@
+const awsService = require("./services/awsService.js");
+
 exports.handler = async (event, context, callback)=>{
+
+    console.log(typeof event.uuid);
+    console.log(typeof event.date);
+
+    const tableName = process.env.MASS_TABLE_NAME;
+
+    const response = await awsService.dynamodb.putItem(tableName, event);
+
     callback(null, {
-        statusCode : 200,
-        data: event
+        statusCode : response.$response.statusCode,
+        data: response.$response.data,
+        error: response.$response.error
     });
 }
