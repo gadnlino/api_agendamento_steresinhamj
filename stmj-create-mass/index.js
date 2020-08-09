47ab480722dd07157ff8e5cebf495903e935a7cb
gadnlinoo <guiavenas@gmail.com>
2020-08-08 23:25:58 -0300

    Buscando missas por intervalo de data em stmj-get-mass

--- stmj-create-mass/index.js
+++ stmj-create-mass/index.js
@@ -1,17 +1,10 @@
 const awsService = require("./services/awsService.js");
 
-exports.handler = async (event, context, callback)=>{
-
-    console.log(typeof event.uuid);
-    console.log(typeof event.date);
+exports.handler = async (event, context, callback) => {
 
     const tableName = process.env.MASS_TABLE_NAME;
 
     const response = await awsService.dynamodb.putItem(tableName, event);
 
-    callback(null, {
-        statusCode : response.$response.statusCode,
-        data: response.$response.data,
-        error: response.$response.error
-    });
+    callback(response.$response.error, response.$response.data,);
 }
--- stmj-get-mass/index.js
+++ stmj-get-mass/index.js
@@ -2,14 +2,29 @@ const awsService = require("./services/awsService.js");
 
 exports.handler = async (event, context, callback) => {
 
-    const { massId } = event;
+    const { massId, start_date, end_date } = event;
 
-    const queryResp = await awsService.dynamodb.queryItems(
-        process.env.MASS_TABLE_NAME,
-        "#id = :value",
-        { "#id": "uuid" },
-        { ":value": massId }
-    );
+    if (massId && (!start_date && !end_date)) {
+        const queryResp = await awsService.dynamodb.queryItems(
+            process.env.MASS_TABLE_NAME,
+            "#id = :value",
+            { "#id": "uuid" },
+            { ":value": massId }
+        );
 
-    callback(null, queryResp.Items[0]);
+        callback(null, queryResp.Items[0]);
+    }
+    else {
+        const queryResp = await awsService.dynamodb.queryItems(
+            process.env.MASS_TABLE_NAME,
+            "#key BETWEEN :value1 AND :value2",
+            { "#key": "date" },
+            {
+                ":value1": start_date,
+                ":value2": end_date
+            }
+        );
+
+        callback(null, queryResp.Items);
+    }
 }
