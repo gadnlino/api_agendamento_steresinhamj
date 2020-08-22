f87481d8d65e8c6b7f839e40cc0394102251b6ab
gadnlinoo <guiavenas@gmail.com>
2020-08-22 00:52:38 -0300

    Alterando endpoint create-manual-scheduling

--- stmj-create-manual-scheduling/index.js
+++ stmj-create-manual-scheduling/index.js
@@ -2,7 +2,10 @@ const awsService = require("./services/awsService.js");
 const uuid = require("uuid");
 
 exports.handler = async (event, context, callback) => {
+    
     const { massId, name, email, phone } = event;
+    
+    let error;
 
     const queryRespMass = await awsService.dynamodb.queryItems(
         process.env.MASS_TABLE_NAME,
@@ -18,50 +21,25 @@ exports.handler = async (event, context, callback) => {
         callback(error, null);
     }
 
-    let personId;
+    let person = {
+        name,
+        email,
+        phone
+    };
 
-    const queryRespPerson = await awsService.dynamodb.queryItems(
-        process.env.PERSON_TABLE_NAME,
-        "#name = :name OR #email = :email OR #phone = :phone",
-        { "#name": "name", "#email": "email", "#phone": "phone" },
-        { ":name": name, ":email": email, ":phone": phone }
-    );
-
-    let person = queryRespPerson.Items[0];
-
-    let error;
-
-    if (!person) {
-        personId = uuid.v1();
-
-        person = {
-            uuid: personId,
-            name,
-            email,
-            phone,
-            masses: []
-        };
-
-        await awsService.dynamodb.putItem(process.env.PERSON_TABLE_NAME, person);
-    }
-
-    const personMasses = person.masses || [];
     const massPeople = mass.people || [];
 
-    if (massPeople.length <= parseInt(process.env.MASS_SCHEDULE_LIMIT) &&
-        !personMasses.find(m => m.uuid === massId) &&
-        !massPeople.find(p => p.uuid === personId)) {
-        const newPersonMasses = [...personMasses, mass];
+    if (massPeople.length <= parseInt(process.env.MASS_SCHEDULE_LIMIT)) {
         const newMassPeople = [...massPeople, person];
 
         try {
 
-            await awsService.dynamodb.updateItem(
-                process.env.PERSON_TABLE_NAME,
-                { "uuid": personId },
-                "set masses = :value",
-                { ":value": newPersonMasses }
-            );
+            // await awsService.dynamodb.updateItem(
+            //     process.env.PERSON_TABLE_NAME,
+            //     { "uuid": personId },
+            //     "set masses = :value",
+            //     { ":value": newPersonMasses }
+            // );
 
             await awsService.dynamodb.updateItem(
                 process.env.MASS_TABLE_NAME,
